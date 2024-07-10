8cd577ee3b1d8336e34d0d20a84b44d8dfc9dc2d
Niles Thompson <dewand.th@gmail.com>
2024-07-10 12:14:33 -0700

    Initial commit for file manager program

---
+++ gui.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul 10 11:52:35 2024
+
+@author: NilesThompson
+"""
+
---
+++ main.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul 10 11:53:07 2024
+
+@author: NilesThompson
+"""
+
