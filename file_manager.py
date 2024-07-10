e54597caf4c4935c0d8c6103c196acd5df0a3e3f
Niles Thompson <dewand.th@gmail.com>
2024-07-10 12:31:15 -0700

    Added Method to organize files by their extensions

--- file_manager.py
+++ file_manager.py
@@ -5,3 +5,25 @@ Created on Wed Jul 10 11:37:28 2024
 @author: NilesThompson
 """
 
+import os
+import shutil
+from collections import defaultdict
+
+# Class to handle file management
+class FileManager:
+    def __init__(self, download_folder):
+        self.download_folder = download_folder
+        self.files = defaultdict(list)
+
+    # Method to scan all files in the download folder
+    def scan_files(self):
+        for root, _, files in os.walk(self.download_folder):
+            for file in files:
+                file_path = os.path.join(root, file)
+                file_ext = os.path.splitext(file)[1].lower()
+                self.files[file_ext].append(file_path)
+
+#Method to organize files by their extensions
+    def organize_files(self):
+        for ext, files in self.files.items():
+            files.sort()
